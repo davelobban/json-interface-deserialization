057fe8167b48a6bbd5e766914313c128f4085d9b
dave <d2_git@lobban-banff.co.uk>
2019-06-17 16:37:20 +0100

    Level1 tests pass with EP and HEP

--- json-interface-deserialization/IValueFor.cs
+++ json-interface-deserialization/IValueFor.cs
@@ -7,7 +7,6 @@ namespace json_interface_deserialization
     public enum Env
     {
         Preprod,
-        Sandbox,
         Production
     }
 
@@ -16,42 +15,24 @@ namespace json_interface_deserialization
         string ValueFor(Env environment);
     }
 
-    public class Level0ValueFor : IValueFor, IEquatable<Level0ValueFor>
+    public class Level0ValueFor : IValueFor//, IEquatable<Level0ValueFor>
     {
         public string Typ = "Level0ValueFor";
+        [JsonProperty]
         private IValueFor _iValueFor;
 
         public string ValueFor(Env environment) => _iValueFor.ValueFor(environment);
-
+        [JsonConstructor]
+        public Level0ValueFor()
+        {
+        }
         public Level0ValueFor(IValueFor iValueFor)
         {
             _iValueFor = iValueFor;
         }
 
-        public bool Equals(Level0ValueFor other)
-        {
-            if (ReferenceEquals(null, other)) return false;
-            if (ReferenceEquals(this, other)) return true;
-            return string.Equals(Typ, other.Typ) && Equals(_iValueFor, other._iValueFor);
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
-            return Equals((Level0ValueFor) obj);
-        }
-
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                return ((Typ != null ? Typ.GetHashCode() : 0) * 397) ^ (_iValueFor != null ? _iValueFor.GetHashCode() : 0);
-            }
-        }
     }
-    //[JsonConverter(typeof(Level1ValueForConverter))]
+
     public class Level1ValueFor : IValueFor, IEquatable<Level1ValueFor>
     {
         [JsonProperty]
@@ -93,30 +74,6 @@ namespace json_interface_deserialization
             }
         }
     }
-
-    //public class Level1ValueForConverter : JsonConverter
-    //{
-    //    public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
-    //    {
-    //        serializer.Serialize(writer, value, typeof(Level1ValueFor));
-    //    }
-
-    //    public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
-    //    {
-    //        //var item = JObject.Load(reader);
-    //        //var ivf = item["_iValueFor"].Value<string>();
-    //        ////return new Level1ValueFor();
-    //        var value = serializer.Deserialize(reader, typeof(Level1ValueFor));
-    //        return value;
-    //    }
-
-    //    public override bool CanConvert(Type objectType)
-    //    {
-    //        return (objectType == typeof(Level1ValueFor));
-    //    }
-    //}
-
-
     public class IValueForConverter : JsonConverter
     {
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
@@ -126,10 +83,8 @@ namespace json_interface_deserialization
 
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
-            //var item = JObject.Load(reader);
-            //var ivf = item["_iValueFor"].Value<string>();
-            ////return new Level1ValueFor();
-            var value = serializer.Deserialize(reader, typeof(EnvironmentPrinter));
+            var item = JObject.Load(reader);
+            var value = serializer.Deserialize(reader, typeof(EnvironmentPrinter));  //This is the ultimate payload in one test but it doesn't cover other scenario implementations and it omits any intermediate 'IValueFor's.
             return value;
         }
 
@@ -138,12 +93,40 @@ namespace json_interface_deserialization
             return (objectType == typeof(IValueFor));
         }
     }
+    public class Level1ValueForConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            serializer.Serialize(writer, value, typeof(Level1ValueFor));
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var item = JObject.Load(reader);
+            if (item["Typ"].Value<string>() == "Level1ValueFor")
+            {
+                if (item["_iValueFor"].Value<JObject>()["Typ"].Value<string>()=="EnvironmentPrinter")
+                return new Level1ValueFor(new EnvironmentPrinter());
+                if (item["_iValueFor"].Value<JObject>()["Typ"].Value<string>() == "HappyEnvironmentPrinter")
+                    return new Level1ValueFor(new HappyEnvironmentPrinter());
+            }
+            var value = serializer.Deserialize(reader, typeof(Level1ValueFor));  //This is the ultimate payload in one test but it doesn't cover other scenario implementations and it omits any intermediate 'IValueFor's.
+            
+            return value;
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return (objectType == typeof(Level1ValueFor));
+        }
+    }
 
     public class EnvironmentPrinter : IValueFor, IEquatable<EnvironmentPrinter>
     {
-        public string Typ = "EnvironmentPrinter";
+        public virtual string Typ => "EnvironmentPrinter";
 
-        public string ValueFor(Env environment) => $"This is {environment}";
+        public virtual string ValueFor(Env environment) => $"This is {environment}";
+
 
         public bool Equals(EnvironmentPrinter other)
         {
@@ -164,5 +147,13 @@ namespace json_interface_deserialization
         {
             return (Typ != null ? Typ.GetHashCode() : 0);
         }
+    }
+    public class HappyEnvironmentPrinter : EnvironmentPrinter
+    {
+        public override string Typ => "HappyEnvironmentPrinter";
+
+        public override string ValueFor(Env environment) => $"Happy to  say, This is {environment}";
+
+        
     }
 }
--- json-interface-deserializationTests/ValueForTests.cs
+++ json-interface-deserializationTests/ValueForTests.cs
@@ -11,59 +11,135 @@ namespace Tests
         {
         }
 
-        [Test]
-        public void ThreeLevelsNested_PrintsExpectedValue()
-        {
-            var printer = new EnvironmentPrinter();
-            var level1 = new Level1ValueFor(printer);
-            var level0 = new Level0ValueFor(level1);
-            Assert.AreEqual("This is Preprod", level0.ValueFor(Env.Preprod));
-        }
-
-        [Test]
-        public void SerialiseEnvironmentPrinter_EqualsUnserialisedValue()
-        {
-            var printer = new EnvironmentPrinter();
-            
-            var serialised = JsonConvert.SerializeObject(printer);
-            var deserialised = JsonConvert.DeserializeObject<EnvironmentPrinter>(serialised);
-
-            Assert.AreEqual(printer, deserialised);
-        }
-
-        [Test]
-        public void SerialiseOneLevelsNested_EqualsUnserialisedValue()
-        {
-            var printer = new EnvironmentPrinter();
-            var level1 = new Level1ValueFor(printer);
-
-            //var settings = new JsonSerializerSettings()
-            //{
-            //    TypeNameHandling = TypeNameHandling.Auto
-            //};
-            var serialised = JsonConvert.SerializeObject(level1, Formatting.Indented,
-                new JsonSerializerSettings
-                {
-                    ReferenceLoopHandling = ReferenceLoopHandling.Serialize
-                });
-            var deserialised = JsonConvert.DeserializeObject<Level1ValueFor>(serialised, new IValueForConverter());
-
-            Assert.AreEqual(level1, deserialised);
-            Assert.AreEqual("This is Preprod", level1.ValueFor(Env.Preprod));
-        }
-
         //[Test]
-        //public void SerialiseThreeLevelsNested_EqualsUnserialisedValue()
+        //public void ThreeLevelsNested_PrintsExpectedValue()
         //{
         //    var printer = new EnvironmentPrinter();
         //    var level1 = new Level1ValueFor(printer);
         //    var level0 = new Level0ValueFor(level1);
-
-        //    var serialised=JsonConvert.SerializeObject(level0);
-        //    var deserialised = JsonConvert.DeserializeObject<Level0ValueFor>(serialised);
-
-        //    Assert.AreEqual(level0, deserialised);
+        //    Assert.AreEqual("This is Preprod", level0.ValueFor(Env.Preprod));
         //}
+
+        //[Test]
+        //public void SerialiseEnvironmentPrinter_EqualsUnserialisedValue()
+        //{
+        //    var printer = new EnvironmentPrinter();
+            
+        //    var serialised = JsonConvert.SerializeObject(printer);
+        //    var deserialised = JsonConvert.DeserializeObject<EnvironmentPrinter>(serialised);
+
+        //    Assert.AreEqual(printer, deserialised);
+        //}
+
+        [Test]
+        public void SerialiseOneLevelsNested_ReturnsExpectedValue()
+        {
+            var printer = new EnvironmentPrinter();
+            var level1 = new Level1ValueFor(printer);
+            var serialised = Serialise(level1);
+            var deserialised = JsonConvert.DeserializeObject<Level1ValueFor>(serialised, new Level1ValueForConverter());
+
+            Assert.AreEqual(level1, deserialised);
+            Assert.AreEqual("This is Preprod", deserialised.ValueFor(Env.Preprod));
+        }
+
+        [Test]
+        public void HappySerialiseOneLevelsNested_ReturnsExpectedValue()
+        {
+            var printer = new HappyEnvironmentPrinter();
+            var level1 = new Level1ValueFor(printer);
+            var serialised = Serialise(level1);
+            var deserialised = JsonConvert.DeserializeObject<Level1ValueFor>(serialised, new Level1ValueForConverter());
+
+            Assert.AreEqual(level1, deserialised);
+            Assert.AreEqual("Happy to  say, This is Preprod", deserialised.ValueFor(Env.Preprod));
+        }
+
+        private static string Serialise(IValueFor level1)
+        {
+            var serializeObject = JsonConvert.SerializeObject(level1, Formatting.Indented,
+                new JsonSerializerSettings
+                {
+                    ReferenceLoopHandling = ReferenceLoopHandling.Serialize
+                });
+            var serialised = serializeObject;
+            return serialised;
+        }
+
+        /*
+        [Test]
+        public void SerialiseTwoLevelsNested_EqualsUnserialisedValue()
+        {
+            var printer = new EnvironmentPrinter();
+            var level1 = new Level1ValueFor(printer);
+            var level0 = new Level0ValueFor(level1);
+
+            var serialised = JsonConvert.SerializeObject(level0, Formatting.Indented,
+                new JsonSerializerSettings
+                {
+                    ReferenceLoopHandling = ReferenceLoopHandling.Serialize
+                });
+            var deserialised = JsonConvert.DeserializeObject<Level0ValueFor>(serialised, new IValueForConverter());
+
+            //Assert.AreEqual(level0, deserialised);
+            Assert.AreEqual("This is Preprod", deserialised.ValueFor(Env.Preprod));
+        }
+
+
+        [Test]
+        public void SerialiseFourLevelsNested_EqualsUnserialisedValue()
+        {
+            var printer = new EnvironmentPrinter();
+            var level1 = new Level1ValueFor(printer);
+            var level0 = new Level0ValueFor(level1);
+            var level00 = new Level0ValueFor(level0);
+            var level000 = new Level0ValueFor(level00);
+
+            var serialised = JsonConvert.SerializeObject(level000, Formatting.Indented,
+                new JsonSerializerSettings
+                {
+                    ReferenceLoopHandling = ReferenceLoopHandling.Serialize
+                });
+            var deserialised = JsonConvert.DeserializeObject<Level0ValueFor>(serialised, new IValueForConverter());
+
+            //Assert.AreEqual(level0, deserialised);
+            Assert.AreEqual("This is Preprod", deserialised.ValueFor(Env.Preprod));
+        }
+
+
+        [Test]
+        public void HappySerialiseFourLevelsNested_EqualsUnserialisedValue()
+        {
+            var printer = new HappyEnvironmentPrinter();
+            var level1 = new Level1ValueFor(printer);
+            var level0 = new Level0ValueFor(level1);
+            var level00 = new Level0ValueFor(level0);
+            var level000 = new Level0ValueFor(level00);
+
+            var serialised = JsonConvert.SerializeObject(level000, Formatting.Indented,
+                new JsonSerializerSettings
+                {
+                    ReferenceLoopHandling = ReferenceLoopHandling.Serialize
+                });
+            var deserialised = JsonConvert.DeserializeObject<Level0ValueFor>(serialised, new IValueForConverter());
+
+            Assert.AreEqual("Happy to  say, This is Preprod", deserialised.ValueFor(Env.Preprod));
+        }
+
+
+        //[Test]
+            //public void SerialiseThreeLevelsNested_EqualsUnserialisedValue()
+            //{
+            //    var printer = new EnvironmentPrinter();
+            //    var level1 = new Level1ValueFor(printer);
+            //    var level0 = new Level0ValueFor(level1);
+
+            //    var serialised=JsonConvert.SerializeObject(level0);
+            //    var deserialised = JsonConvert.DeserializeObject<Level0ValueFor>(serialised);
+
+            //    Assert.AreEqual(level0, deserialised);
+            //}
+        */
     }
 
 
